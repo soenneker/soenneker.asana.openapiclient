d50c2bb8fe8e1a3937f173b3998ea67b74786479
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-07-01 18:59:00 +0000

    Automated update

--- src/Soenneker.Asana.OpenApiClient/Workspaces/Item/WithWorkspaceGItemRequestBuilder.cs
+++ src/Soenneker.Asana.OpenApiClient/Workspaces/Item/WithWorkspaceGItemRequestBuilder.cs
@@ -6,6 +6,7 @@ using Microsoft.Kiota.Abstractions;
 using Soenneker.Asana.OpenApiClient.Models;
 using Soenneker.Asana.OpenApiClient.Workspaces.Item.AddUser;
 using Soenneker.Asana.OpenApiClient.Workspaces.Item.Agents;
+using Soenneker.Asana.OpenApiClient.Workspaces.Item.Ai_studio;
 using Soenneker.Asana.OpenApiClient.Workspaces.Item.Audit_log_events;
 using Soenneker.Asana.OpenApiClient.Workspaces.Item.Custom_fields;
 using Soenneker.Asana.OpenApiClient.Workspaces.Item.Events;
@@ -39,6 +40,11 @@ namespace Soenneker.Asana.OpenApiClient.Workspaces.Item
         public global::Soenneker.Asana.OpenApiClient.Workspaces.Item.Agents.AgentsRequestBuilder Agents
         {
             get => new global::Soenneker.Asana.OpenApiClient.Workspaces.Item.Agents.AgentsRequestBuilder(PathParameters, RequestAdapter);
+        }
+        /// <summary>The ai_studio property</summary>
+        public global::Soenneker.Asana.OpenApiClient.Workspaces.Item.Ai_studio.Ai_studioRequestBuilder Ai_studio
+        {
+            get => new global::Soenneker.Asana.OpenApiClient.Workspaces.Item.Ai_studio.Ai_studioRequestBuilder(PathParameters, RequestAdapter);
         }
         /// <summary>The audit_log_events property</summary>
         public global::Soenneker.Asana.OpenApiClient.Workspaces.Item.Audit_log_events.Audit_log_eventsRequestBuilder Audit_log_events
